2608d1bdfd97de52322df9717b8f3b3fe78c8f85
thiagocorta <thiago.tomas.corta@accenture.com>
2019-09-04 16:09:06 -0300

    empezando tp1

---
+++ TrabajoPractico1/TrabajoPractico1/Calculadora.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPractico1
+{
+    class Calculadora
+    {
+        public double Operar(Numero n1, Numero n2, string operador)
+        {
+            
+            double cuenta = 0;
+            switch (ValidarOperador(operador))
+            {
+                case "+":
+                    cuenta = n1 + n2;
+                    break;
+                case "-":
+                    cuenta = n1 - n2;
+                    break;
+                case "/":
+                    cuenta = n1 / n2;
+                    break;
+                case "*":
+                    cuenta = n1 * n2;
+                    break;
+            }
+            return cuenta;
+        }
+
+        private static string ValidarOperador(string operador)
+        {
+                
+            if(operador!="-" && operador!="+" && operador!="/" && operador != "*")
+            {
+                return "+";
+            }
+
+            return operador;
+        }
+    }
+}
---
+++ TrabajoPractico1/TrabajoPractico1/Numero.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPractico1
+{
+    class Numero
+    {
+        private double numero;
+
+        public Numero()
+        {
+
+        }
+
+        public Numero(double num)
+        {
+            this.numero = num;
+        }
+
+        public Numero(string strNum)
+        {
+            double.TryParse(strNum, out this.numero);
+        }
+
+        public double ValidarNumero(string strNum)
+        {
+            double aux = 0;
+            double.TryParse(strNum, out aux);
+            return aux;
+        }
+
+
+        public double SetNumero { set { this.numero = value; } }
+        public static double operator +(Numero n1, Numero n2)
+        {
+            return n1.numero + n2.numero;
+        }
+
+        public static double operator -(Numero n1, Numero n2)
+        {
+            return n1.numero - n2.numero;
+        }
+
+        public static double operator /(Numero n1, Numero n2)
+        {
+            if (n2.numero != 0)
+            {
+                return n1.numero / n2.numero;
+            }
+            else
+            {
+                return double.MinValue;
+            }
+        }
+
+        public static double operator *(Numero n1, Numero n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        public string DecimalBinario(double x)
+        {
+            long m = BitConverter.DoubleToInt64Bits(x);
+            return Convert.ToString(m, 2);
+        }
+
+        public double BinarioDecimal(string x)
+        {
+            return BitConverter.Int64BitsToDouble(Convert.ToInt64(x, 2));
+
+        }
+
+    }
+}
