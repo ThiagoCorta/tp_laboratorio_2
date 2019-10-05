74c24da7f029964303df9eabe9166b50635c19b2
Thiago Corta <thiago.corta@gmail.com>
2019-10-05 20:49:07 -0300

    tp2

---
+++ TrabajoPractico2/Entidades/Calculadora.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        /// <summary>
+        /// Recive dos instancias de la clase Numero y un string.
+        /// Realiza la operacion matematica segun el signo que se paso como parametro (string)
+        /// Retorna el valor de la operacion
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        public double Operar(Numero n1, Numero n2, string operador)
+        {
+
+            double cuenta = 0;
+            switch (ValidarOperador(operador))
+            {
+                case "+":
+                    cuenta = n1 + n2;
+                    break;
+                case "-":
+                    cuenta = n1 - n2;
+                    break;
+                case "/":
+                    cuenta = n1 / n2;
+                    break;
+                case "*":
+                    cuenta = n1 * n2;
+                    break;
+            }
+            return cuenta;
+        }
+
+        /// <summary>
+        /// Recive un string y valida cual de los operadores es, + - / * etc y lo devuelve.
+        /// Caso contrario que no sea ninguno de esos, devuelve +.
+        /// </summary>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        private static string ValidarOperador(string operador)
+        {
+           
+            if (operador != "-" && operador != "+" && operador != "/" && operador != "*")
+            {
+                return "+";
+            }
+
+            return operador;
+        }
+
+    }
+}
---
+++ TrabajoPractico2/Entidades/Dulce.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Dulce : Producto
+    {
+        public Dulce(EMarca marca, string codigo, ConsoleColor color) : base(codigo,marca,color)
+        {
+        }
+
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+
+
+        /// <summary>
+        /// sobrescribe el mostrar de producto, mostrando el producto / calorias
+        /// </summary>
+        /// <returns>el producto dulce</returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"CALORIAS : {this.CantidadCalorias}");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
